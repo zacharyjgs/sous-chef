416696a15cb4c698ff8b10dcc70f7b37a59abbdd
Zachary Golan-Strieb <zacharyjgs@macbook-pro.lan>
2020-06-23 23:27:58 -0700

    Finish implementing speech recognition

--- api/app.py
+++ api/app.py
@@ -1,8 +1,36 @@
 import time
-from flask import Flask
+from flask import Flask, request, jsonify
+from flask_cors import CORS
+import transformers as tr
+import torch
+from pydub import AudioSegment
+from pydub.playback import play
+from io import BytesIO
+import speech_recognition as sr
 
 app = Flask(__name__)
+CORS(app)
 
-@app.route('/time')
-def get_current_time():
-    return {'time': time.time()}
+tokenizer = tr.AutoTokenizer.from_pretrained("ktrapeznikov/albert-xlarge-v2-squad-v2")
+model = tr.AutoModelForQuestionAnswering.from_pretrained("ktrapeznikov/albert-xlarge-v2-squad-v2")
+
+@app.route('/ask', methods = ['POST'])
+def get_answer():
+    question, text = request.form['question'], request.form['recipe']
+    recording = request.files['recording']
+    sound = AudioSegment.from_file(recording, codec="opus")
+    sound_file_name = 'temp'
+    sound.export(sound_file_name, format = 'wav')
+    recognizer = sr.Recognizer()
+    with sr.AudioFile(sound_file_name) as audio:
+        listened = recognizer.listen(audio)
+    content = recognizer.recognize_google(listened)
+    encoding = tokenizer.encode_plus(question, text)
+    input_ids, token_type_ids = encoding["input_ids"], encoding["token_type_ids"]
+    start_scores, end_scores = model(torch.tensor([input_ids]), token_type_ids=torch.tensor([token_type_ids]))
+    all_tokens = tokenizer.convert_ids_to_tokens(input_ids)
+    answer_tokens = all_tokens[torch.argmax(start_scores) : torch.argmax(end_scores) + 1]
+    answer = tokenizer.convert_tokens_to_string(answer_tokens)
+    if answer == '[CLS]':
+        answer = "i don't know. sorry, boss!"
+    return {'answer': answer}
--- src/App.js
+++ src/App.js
@@ -1,26 +1,96 @@
 import React from 'react';
-import logo from './logo.svg';
+import { ReactMic } from 'react-mic';
+import logo from './sous_chef_robot3.svg';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      answer: null,
+      recipe: "",
+      question: "",
+      record: false,
+      recording: null
+    };
+  }
+
+  submitQuery = (event) => {
+    var form = new FormData();
+    form.append("recording", this.state.recording.blob)
+    form.set("recipe", this.state.recipe)
+    form.set("question", this.state.question)
+
+    fetch("/ask", {
+        method: "POST",
+        body: form
+      }
+    ).then(response => {
+        return response.json()
+      }
+    ).then(data => {
+        this.setState({
+          answer: data.answer
+        });
+      }
+    );
+  }
+
+  onStop = (recording) => {
+    this.setState({
+      recording: recording
+    });
+  }
+
+  onData = (recording) => {
+  }
+
+  startRecording = () => {
+    this.setState({ record: true });
+  }
+
+  stopRecording = () => {
+    this.setState({ record: false });
+  }
+
+  onChangeRecipe = (event) => {
+    this.setState({
+      recipe: event.target.value
+    });
+  }
+
+  onChangeQuestion = (event) => {
+    this.setState({
+      question: event.target.value
+    });
+  }
+
+  render() {
+    return (
+      <div className="App">
+        <img src={logo} className="App-logo" alt="logo"/>
+        <p className="App-title">Sous Chef</p>
+        <p>What's cookin'? (Paste recipe below)</p>
+        <textarea name="recipe" value={this.state.recipe} onChange={this.onChangeRecipe} className="recipeBox"/>
+        <p className="questionInstructions">Ask away, Chef! (Paste question below)</p>
+        <ReactMic
+          record={this.state.record}
+          onStop={this.onStop}
+          onData={this.onData}
+          strokeColor="#000000"
+          backgroundColor="#FFFFFF"
+          className="recorder"/>
+        <button onClick={this.startRecording} type="button" className="Button-start">Start</button>
+        <button onClick={this.stopRecording} type="button" className="Button-stop">Stop</button>
+        <br />
+        <input type="text" name="question" value={this.state.question} onChange={this.onChangeQuestion} className="questionBox"/>
+        <button onClick={this.submitQuery} className="Button-ask">Ask</button>
+        {this.state.answer ? <p className="answer">"{this.state.answer}"</p> : <p></p>}
+      </div>
+    );
+  }
 }
 
 export default App;
